d64150cbc2fb7b5ed450be1040fe019e52562127
Kolosocc <kolosovda@mer.ci.nsu.ru>
2025-03-09 16:03:11 +0700

    Пожалуёста глянь что можно сделать с картинками

---
+++ Geo_worker/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Geo_worker;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
